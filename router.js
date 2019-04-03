75194277c5a7a0159595ca59d9ae966ef7cab823
HN <viethung1996@gmail.com>
2019-04-03 15:53:45 -0400

    handle POST requests

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
   //handle is passed from index to server then finally here
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === "function") {
-    handle[pathname](response);
+    handle[pathname](response, postData);
   } else {
     console.log("No request handle found for " + pathname);
     response.writeHead(404, { "Content-Type": "text/plain" });
