14bb863d45a10fd86dd7df49ed7c11fb4877d12f
HN <viethung1996@gmail.com>
2019-04-03 11:38:40 -0400

    add blocking operation to start

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,11 @@
 function start() {
   console.log("Request handler 'start' was called.");
+
+  function sleep(milliSecs) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSecs);
+  }
+  sleep(10000);
   return "Hello Start";
 }
 
