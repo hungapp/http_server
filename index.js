d1cb989a84f88a5c6e9c4c4b38c1be98eb35503e
HN <viethung1996@gmail.com>
2019-04-03 11:13:21 -0400

    make event handlers respond

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var server = require("./server");
 var router = require("./router");
-server.start(router.route);
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+
+// "/": requestHandles.start,
+// "/start": requestHandles.start,
+// "/upload": requestHandlers.upload
+
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathname) {
+function route(handle, pathname) {
+  //handle is passed from index to server then finally here
   console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === "function") {
+    return handle[pathname]();
+  } else {
+    console.log("No request handle found for " + pathname);
+    return "404 Not found";
+  }
 }
 
 exports.route = route;
