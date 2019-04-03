17660d440fa9fc89b388597323b89adcb70dc04f
HN <viethung1996@gmail.com>
2019-04-03 13:09:44 -0400

    repond reques handlers with non-blocking operations

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   //handle is passed from index to server then finally here
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === "function") {
-    return handle[pathname]();
+    handle[pathname](response);
   } else {
     console.log("No request handle found for " + pathname);
-    return "404 Not found";
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write("404 Not found");
+    response.end();
   }
 }
 
